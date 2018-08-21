ccfe88e8dd9391316931d29b7fb10991ddc2f7d9
ngoclinh.truong <ngoclinh.truong@ved.com.vn>
2018-08-21 11:16:27 +0700

    Add Playground project, up-to-date android with kotlin support

---
+++ Playground/app/src/main/java/samples/linhtruong/com/playground/java/MainActivity.java
@@ -0,0 +1,23 @@
+package samples.linhtruong.com.playground.java;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import samples.linhtruong.com.playground.R;
+
+/**
+ * CLASS DESCRIPTION
+ *
+ * @author linhtruong
+ * @date 8/21/18 - 10:17.
+ * @organization VED
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ Playground/app/src/main/java/samples/linhtruong/com/playground/kotlin/MainActivity.kt
@@ -0,0 +1,13 @@
+package samples.linhtruong.com.playground.kotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import samples.linhtruong.com.playground.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
